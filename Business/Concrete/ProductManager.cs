c9ebddd64bbf0650181ab06e8dc3bad72e6a6374
Akile Oday <akile.oday@emu.edu.tr>
2021-04-26 23:44:08 +0300

    26.4.2021

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,46 @@
+﻿using Business.Abstract;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+	public class ProductManager : IProductService
+	{
+		IProductDal _productDal;
+
+		public ProductManager(IProductDal productDal)
+		{
+			_productDal = productDal;
+		}
+
+		public IResult Add(Product product)
+		{
+			//is kurallari yazilir
+			
+			if(product.CategoryId==1)
+			{
+				
+				return new ErrorResult("Bu katagoriye urun kabul edilmiyor");
+			}
+			else 
+			{
+				_productDal.Add(product);
+				return new SuccessResult("Urun eklendi");
+			}
+		}
+
+		public IDataResult<List<Product>> GelAllByCategory(int categoryId)
+		{
+			return new SuccessDataResult<List<Product>>(_productDal.GetAll(p=>p.CategoryId==categoryId), "urunler listelendi");
+		}
+
+		public IDataResult<List<Product>> GetAll()
+		{
+			return new SuccessDataResult<List<Product>>(_productDal.GetAll(),"urunler listelendi");
+		}
+	}
+}
